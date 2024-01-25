e4267aa2ec2bec39d8d19e067c5ec6052125bd07
Bubbwubb <113468117+Bubbwubb@users.noreply.github.com>
2024-01-25 14:14:11 -0600

    First Commit

---
+++ Assignment1/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(600, 300);
+}
+
+function draw() {
+  //much documentation such wow, very circle and incredibly square
+  background(133, 250, 90);
+  circle(height/2, width/4, 240);
+  rect(330, 30, 240, 240);
+}
